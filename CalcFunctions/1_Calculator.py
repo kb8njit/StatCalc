f12593144d816571812f555b589140a4519e4f81
kb8njit <kb8@njit.edu>
2020-07-01 12:20:04 -0400

    Task: Created Calculator class

--- CalcFunctions/1_Calculator.py
+++ CalcFunctions/1_Calculator.py
@@ -5,3 +5,29 @@ from CalcFunctions.Division import division
 from CalcFunctions.Square import square
 from CalcFunctions.SquareRoot import squareroot
 
+class Calculator:
+    result = 0
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def squareroot(self, a):
+        self.result = squareroot(a)
+        return self.result
