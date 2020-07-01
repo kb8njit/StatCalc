8789f43835784bb115e58172dae99c52e91af476
kb8njit <kb8@njit.edu>
2020-07-01 19:11:09 -0400

    Task: Created and added code to Setup.py

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      version='0.0.1',
+      description='Statistics Calculator',
+      authors='Kelly Blackledge and Monica Nelson',
+      author_emails='kb8@njit.edu and mjn28@njit.edu',
+      url='',
+      packages=find_packages(),
+      )
