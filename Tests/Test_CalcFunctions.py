e9ebf009a506951e75cfe88c8e8f0afb0ee841ad
kb8njit <kb8@njit.edu>
2020-07-01 17:52:17 -0400

    Task: Added To Do file

--- Tests/Test_CalcFunctions.py
+++ Tests/Test_CalcFunctions.py
@@ -1,1 +1,12 @@
-from
+import unittest
+import math
+from CalcFunctions.Calculator import Calculator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsNone(self.calculator, Calculator)
